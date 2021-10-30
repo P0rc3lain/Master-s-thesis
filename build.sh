2a5f6462ff6c7298694fe8507148e34cc9c377d4
Mateusz Stompór <mateusz.stompor@gmail.com>
2021-10-31 00:00:44 +0200

    Make the build script fail when the syntax is incorrect

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
+AUX_EXTENSIONS="aux toc out log"
 DIST_DIR=distribution
 AUX_DIR=auxilary
 
-mkdir -p ${DIST_DIR} ${AUX_DIR}
-pdflatex -halt-on-error -output-directory=distribution master-s-thesis.tex 2>&1 1>/dev/null
-AUX_EXTENSIONS="aux toc out log"
+fail() {
+  echo $1
+  exit 1
+}
+
+mkdir -p ${DIST_DIR} ${AUX_DIR} || fail "Could not create output directories"
+pdflatex -halt-on-error -output-directory=distribution master-s-thesis.tex 2>&1 1>/dev/null || fail "Could not build the pdf"
 for EXTENSION in $AUX_EXTENSIONS
 do 
   FILES=$(find $DIST_DIR -type f -regex ".*.$EXTENSION")
   if [[ ! -z "$FILES" ]]
   then
-    mv $FILES $AUX_DIR
+    mv $FILES $AUX_DIR || fail "Could not move the files"
   fi
 done
