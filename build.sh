01abac66b22971b5432de6537c0b81afaab3e1cb
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-05-22 12:32:36 +0200

    Add timestamp to the pdf name

--- build.sh
+++ build.sh
@@ -4,22 +4,19 @@
 #  Copyright © 2022 Mateusz Stompór. All rights reserved.
 #
 
-AUX_EXTENSIONS="aux toc out log lof lol fdb_latexmk fls gz"
 DIST_DIR=distribution
-AUX_DIR=auxilary
+BUILD_DATE=$(TZ=Europe/Warsaw date +"%m%d%H%M")
+AUTHOR_NAME="Mateusz Stompór"
+TITLE="Projekt i Implementacja Silnika Grafiki Trójwymiarowej w Środowisku Sprzętowym Apple"
+FILE_NAME="${AUTHOR_NAME} - ${TITLE} - ${BUILD_DATE}"
 
 fail() {
   echo $1
   exit 1
 }
 
-mkdir -p ${DIST_DIR} ${AUX_DIR} || fail "Could not create output directories"
-pdflatex -halt-on-error -output-directory=distribution master-s-thesis.tex 2>&1 1>/dev/null || fail "Could not build the pdf"
-for EXTENSION in $AUX_EXTENSIONS
-do 
-  FILES=$(find $DIST_DIR -type f -regex ".*.$EXTENSION")
-  if [[ ! -z "$FILES" ]]
-  then
-    mv $FILES $AUX_DIR || fail "Could not move the files"
-  fi
-done
+mkdir -p ${DIST_DIR} || fail "Could not create output directories"
+pdflatex -halt-on-error \
+         -jobname="${FILE_NAME}" \
+         -output-directory=${DIST_DIR} \
+         master-s-thesis.tex 2>&1 1>/dev/null || fail "Could not build the pdf"
