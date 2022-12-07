a0143f414c79918508b7b8e0bc7ab738732d4d18
Mateusz Stompór <mateusz.stompor@gmail.com>
2022-12-07 22:50:28 +0100

    Explain style

---
+++ code/PNKeyframeAnimation.swift
@@ -0,0 +1,25 @@
+//
+//  Copyright © 2022 Mateusz Stompór. All rights reserved.
+//
+
+public struct PNKeyframeAnimation<T> {
+    public var keyFrames: [T]
+    public let times: [PNTimePoint]
+    public let maximumTime: PNTimeInterval
+    public init(keyFrames: [T],
+                times: [PNTimePoint],
+                maximumTime: PNTimeInterval) {
+        assertEqual(times.count, keyFrames.count)
+        assertSorted(times)
+        assertNotEmpty(times)
+        assertGreaterOrEqual(maximumTime, times[times.count - 1])
+        self.keyFrames = keyFrames
+        self.times = times
+        self.maximumTime = maximumTime
+    }
+    static public func `static`(from value: T) -> PNKeyframeAnimation<T> {
+        PNKeyframeAnimation<T>(keyFrames: [value],
+                               times: [0],
+                               maximumTime: 1)
+    }
+}
