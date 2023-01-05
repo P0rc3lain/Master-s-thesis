acf680c10715c61093544b39dda089fa164f8b9b
Mateusz Stompór <mateusz.stompor@gmail.com>
2023-01-05 13:47:47 +0100

    Improve captions

--- code/code_sharing/Camera.h
+++ code/code_sharing/Camera.h
@@ -1,5 +1,3 @@
-#pragma once
-
 struct CameraUniforms {
     simd_float4x4 projectionMatrix;
     int index;
--- code/code_sharing/PNSceneDescription.swift
+++ code/code_sharing/PNSceneDescription.swift
@@ -1,37 +1,9 @@
-import Metal
+[...]
 import PNShared
 
 public final class PNSceneDescription {
-    // MARK: - Capacity A
-    public var entities = PNEntityTree()
-    public var uniforms = [PNWModelUniforms]()
-    public var boundingBoxes = [PNWBoundingBox?]()
-    // MARK: - Capacity B
-    public var models = [PNModelReference]()
-    // MARK: - Capacity C
-    public var animatedModels = [PNAnimatedModelReference]()
-    public var paletteOffset = [PNIndex]()
-    // MARK: - Capacity D
-    public var meshes = [PNMesh]()
-    // MARK: - Capacity E
-    public var skeletons = [PNSkeleton]()
-    // MARK: - Capacity F
-    public var palettes = [PNBLTransform]()
-    // MARK: - Capacity G
-    public var cameras = [PNCamera]()
+    [...]
     public var cameraUniforms = [CameraUniforms]()
-    // MARK: - Capacity H
-    public var omniLights = [OmniLight]()
-    // MARK: - Capacity I
-    public var ambientLights = [AmbientLight]()
-    // MARK: - Capacity J
-    public var directionalLights = [DirectionalLight]()
-    // MARK: - Capacity K
-    public var spotLights = [SpotLight]()
-    // MARK: - Capacity L
-    public var particles = [PNParticleSystem]()
-    // MARK: - Capacity M
-    public var skyMap: MTLTexture?
-    public var activeCameraIdx = PNIndex.nil
+    [...]
 }
 
