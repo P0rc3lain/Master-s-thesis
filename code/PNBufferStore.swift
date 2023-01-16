edf4b42dd53fc17c89ce51af046b78627adac9d9
Mateusz Stompór <mateusz.stompor@gmail.com>
2023-01-16 21:12:20 +0100

    Start gathering materials for rendering section

---
+++ code/PNBufferStore.swift
@@ -0,0 +1,9 @@
+public protocol PNBufferStore {
+    var omniLights: PNAnyDynamicBuffer<OmniLight> { get }
+    var ambientLights: PNAnyDynamicBuffer<AmbientLight> { get }
+    var directionalLights: PNAnyDynamicBuffer<DirectionalLight> { get }
+    var spotLights: PNAnyDynamicBuffer<SpotLight> { get }
+    var cameras: PNAnyDynamicBuffer<CameraUniforms> { get }
+    var modelCoordinateSystems: PNAnyDynamicBuffer<ModelUniforms> { get }
+    var matrixPalettes: PNAnyDynamicBuffer<PNBLTransform> { get }
+}
---
+++ code/PNRenderMask.swift
@@ -0,0 +1,5 @@
+public struct PNRenderMask {
+    let cameras: [[Bool]]
+    let spotLights: [[Bool]]
+    let omniLights: [[Bool]]
+}
