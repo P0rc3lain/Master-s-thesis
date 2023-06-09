86cc64619519c8902daec6b0e763dc6bb7ba9bc9
Mateusz Stompór <mstompor@motionvfx.com>
2023-06-09 16:15:54 +0200

    Further length shortening

--- code/chess/ViewController.swift
+++ code/chess/ViewController.swift
@@ -2,35 +2,21 @@
     guard state.checkState != .stalemate && 
           state.checkState != .checkmate else { return }
     let allNodes = engine.scene.rootNode.all()
-    guard let camera = allNodes.compactMap {
-        $0.data as? PNAnimatedCameraNode
-    }.first else { return }
     var moves = [Move]()
-    let selected = state.selectedPiece
+    let selectedPiece = state.selectedPiece
     let location = CGPoint(x: sender!.location(in: view).x,
                            y: frame.height-sender!.location(in: view).y)
     if state.expectation == .piece {
         let piece = interactionHandler.pickPiece(location: location,
-                                                 camera: camera,
                                                  scene: engine.scene,
                                                  viewframe: frame.size)
-        let pieceS = pieceParser.create(literal: piece?.data.name ?? "")
-        (moves, state) = game.select(piece: pieceS, state: state)
+        (moves, state) = game.select(piece: piece, state: state)
     } else {
         let field = interactionHandler.pickField(location: location,
-                                                 camera: camera,
                                                  scene: engine.scene,
                                                  viewframe: frame.size)
-        let fieldS = fieldParser.create(literal: field?.data.name ?? "")
-        (moves, state) = game.select(piece: fieldS, state: state)
+        (moves, state) = game.select(piece: field, state: state)
     }
-    if state.selectedPiece != selected {
-        if let selected = selected {
-            manipulator.deselect(scene: engine.scene, piece: selected)
-        }
-        if let selectedAfter = state.selectedPiece {
-            manipulator.select(scene: engine.scene, piece: selectedAfter)
-        }
-    }
+    manipulator.selectionState(scene: engine.scene, piece: selected)
     manipulator.performMoves(scene: engine.scene, moves: moves)
 }
